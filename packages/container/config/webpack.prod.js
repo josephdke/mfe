e3c345935b66a960bfe292d1ccf8df6b49f0175b
Jose Dario Cardenas Arango <jose.cardenas@carroya.com>
2022-06-25 12:24:23 -0500

    Added marketing script

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js?[window.getRandomString()]`
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js?[window.getRandomString()]`
             },
             shared: packageJson.dependencies
         })
