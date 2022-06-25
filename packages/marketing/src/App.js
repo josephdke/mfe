b9aaedc6bddbba20e4052bca71718dbec4e31356
Jose Dario Cardenas Arango <jose.cardenas@carroya.com>
2022-06-25 12:27:05 -0500

    Added marketing script

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -10,7 +10,7 @@ const generateClassName = createGenerateClassName({
 });
 
 export default ({ history }) => {
-    return <div>
+    return <div>h
         <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Switch>
